45dd25aa7a6897dd557801dc83016c48d96bcdb4
Daproboi <wahajacca@gmail.com>
2026-02-23 11:25:08 -0800

    Update Values.js

--- Values.js
+++ Values.js
@@ -1,23 +1,7 @@
 const vehicleData = [
-  const vehicleData = [
-  { 
-    "name": "Javelin", 
-    "cash_value": 49500000, 
-    "duped_value": 49500000, 
-    "image": "images/javelin.png" 
-  },
-  { 
-    "name": "Torpedo", 
-    "cash_value": 49000000, 
-    "duped_value": 43500000, 
-    "image": "images/torpedo.png" 
-  },
-  { 
-    "name": "Beignet", 
-    "cash_value": 40000000, 
-    "duped_value": 37000000, 
-    "image": "images/beignet.png" 
-  },
+  { "name": "Javelin", "cash_value": 49500000, "duped_value": 49500000, "image": "images/javelin.png" },
+  { "name": "Torpedo", "cash_value": 49000000, "duped_value": 43500000, "image": "images/torpedo.png" },
+  { "name": "Beignet", "cash_value": 40000000, "duped_value": 37000000, "image": "images/beignet.png" },
   { "name": "Celsior", "cash_value": 30000000, "duped_value": 28000000 },
   { "name": "Proto-8", "cash_value": 30000000, "duped_value": 30000000 },
   { "name": "Power 1", "cash_value": 23000000, "duped_value": 23000000 },
